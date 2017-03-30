ee993490cd95287d9c730b2506c0f64ce8d0b854
Eva Lai <evalai.dc@gmail.com>
2017-03-30 11:28:31 -0400

    rendered our cart for view in the app

--- shopping-cart/src/components/App.js
+++ shopping-cart/src/components/App.js
@@ -2,18 +2,12 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../stylesheets/App.css';
 
+import Cart from '../containers/Cart'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Cart />
     );
   }
 }
