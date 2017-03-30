5df988dcf69c4a297c35e0b34475f6d7cbabebfc
Eva Lai <evalai.dc@gmail.com>
2017-03-30 11:32:37 -0400

    added app component

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './stylesheets/index.css';
 
+//provider is a special type of component,
+// wrap our App component with a store that encapsulates local state
+// within App components
+import { Provider } from 'react-redux'
+import Store from './Store'
+
+const StoreInstance = Store()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={StoreInstance}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
